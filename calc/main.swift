7b4ddedf6091f2c71e387840f9bebdf0bd8b2f03
mac <mac@192-168-1-173.tpgi.com.au>
2019-04-04 16:20:25 +1100

    Final code

--- calc/main.swift
+++ calc/main.swift
@@ -11,6 +11,66 @@ import Foundation
 var args = ProcessInfo.processInfo.arguments
 args.removeFirst() // remove the name of the program
 
-print(Int(args[0])!)
+func check(args:[String]) -> Bool {
+    for (index,value) in args.enumerated() {
+        if index % 2 == 1 {
+            if(value != "x" && value != "/" && value != "%" && value != "+" && value != "-"){
+                return false
+            }
+        }
+    }
+    return true
+}
 
-// This is test 
+var result = 0
+func calc(args:[String]) -> Int {
+    while args.count > 1 {
+        if args.contains("x") || args.contains("/") || args.contains("%") {
+            for (index,value) in args.enumerated() {
+                if value == "x" || value == "/" || value == "%" {
+                    if value == "x"{
+                        result = Int(args[index - 1])! * Int(args[index + 1])!
+                    } else if value == "/" {
+                        result = Int(args[index - 1])! / Int(args[index + 1])!
+                    } else if value == "%" {
+                        result = Int(args[index - 1])! % Int(args[index + 1])!
+                    }
+                    var tmpArr = args
+                    tmpArr.remove(at: index)
+                    tmpArr.insert(String(result), at: index)
+                    tmpArr.remove(at: index + 1)
+                    tmpArr.remove(at: index - 1)
+                    return calc(args: tmpArr)
+                }
+            }
+        }
+        else {
+            for (index,value) in args.enumerated() {
+                if value == "+" || value == "-"{
+                    if value == "+"{
+                        result = Int(args[index - 1])! + Int(args[index + 1])!
+                    } else if value == "-"{
+                        result = Int(args[index - 1])! - Int(args[index + 1])!
+                    }
+                    var tmpArr = args
+                    tmpArr.remove(at: index)
+                    tmpArr.insert(String(result), at: index)
+                    tmpArr.remove(at: index + 1)
+                    tmpArr.remove(at: index - 1)
+                    return calc(args: tmpArr)
+                }
+            }
+        }
+    }
+    return result
+}
+
+if (args.count == 1) {
+    print(Int(args.first!)!)
+} else {
+    if(!check(args: args)) {
+        exit(1)
+    } else {
+        print(calc(args: args))
+    }
+}
